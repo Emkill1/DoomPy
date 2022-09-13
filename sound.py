649d460d9148796c57f7d548fc88a24aad1bd8dc
Emkill1 <theemkilllol@gmail.com>
2022-09-14 00:09:38 +0300

    v4

---
+++ sound.py
@@ -0,0 +1,10 @@
+import pygame as pg
+
+
+class Sound:
+    def __init__(self, game):
+        self.game = game
+        pg.mixer.init()
+        self.path = 'resources/sound/'
+        self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
+        self.shotgun.set_volume(0.1)
